6e6a578ecbfe3ba6cbd1de89462e1633436addf2
AenuHub <jesuskane92@gmail.com>
2024-12-23 15:50:50 +0300

    create BaseModel.cs

---
+++ W12_Survivor/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace W12_Survivor.Models;
+
+public class BaseModel
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
