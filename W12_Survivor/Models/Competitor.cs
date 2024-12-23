5c8cc09df431091157cd5b9b4a1efcde719989c5
AenuHub <jesuskane92@gmail.com>
2024-12-23 15:10:14 +0300

    create Competitor.cs

--- W12_Survivor/Models/Competitor.cs
+++ W12_Survivor/Models/Competitor.cs
@@ -1,6 +1,16 @@
-﻿namespace W12_Survivor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W12_Survivor.Models;
 
 public class Competitor
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public bool IsDeleted { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
 }
