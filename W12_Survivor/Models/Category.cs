94c0a3336d19d1e5f6972d773a6aab89e44a945d
AenuHub <jesuskane92@gmail.com>
2024-12-23 15:10:07 +0300

    create Category.cs

--- W12_Survivor/Models/Category.cs
+++ W12_Survivor/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace W12_Survivor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W12_Survivor.Models;
 
 public class Category
 {
-    
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public bool IsDeleted { get; set; }
+    public IEnumerable<Competitor> Competitors { get; set; }
 }
