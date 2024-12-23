49098728e6428cf43d3d0b04439db3ecb8b2f06b
AenuHub <jesuskane92@gmail.com>
2024-12-23 15:51:06 +0300

    inherit BaseModel

--- W12_Survivor/Models/Category.cs
+++ W12_Survivor/Models/Category.cs
@@ -1,14 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace W12_Survivor.Models;
 
-namespace W12_Survivor.Models;
-
-public class Category
+public class Category : BaseModel
 {
-    [Key]
-    public int Id { get; set; }
     public string Name { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
-    public bool IsDeleted { get; set; }
     public IEnumerable<Competitor> Competitors { get; set; }
 }
